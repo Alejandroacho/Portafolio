86c4cef10d120570a5ceca878e4d3de8c469256b
Alejandro <alejandroacho@hotmail.com>
2020-08-12 23:12:37 +0200

    Route | Refactored

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/project-details', function () {
-    return view('projects.portfolio-details');
-});
-
+Route::view('/', 'home');
+Route::view('/project-details', 'projects.portfolio-details');
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
