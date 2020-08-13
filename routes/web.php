4b7572d0e8da9e8e650a0de42d2d04ce4c53c445
Alejandro <alejandroacho@hotmail.com>
2020-08-13 12:47:05 +0200

    Views | Construccion view added

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'index');
-Route::view('/trace', 'projects.trace');
+Route::view('/trace', 'projects.construccion');
+Route::view('/blog', 'projects.construccion');
+Route::view('/sudoku', 'projects.construccion');
+Route::view('/kvirus', 'projects.construccion');
+Route::view('/katas', 'projects.construccion');
+Route::view('/notalone', 'projects.construccion');
+/*Route::view('/trace', 'projects.trace');
 Route::view('/blog', 'projects.blog');
 Route::view('/sudoku', 'projects.sudoku');
 Route::view('/kvirus', 'projects.kvirus');
 Route::view('/katas', 'projects.katas');
-Route::view('/notalone', 'projects.notalone');
+Route::view('/notalone', 'projects.notalone');*/
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
