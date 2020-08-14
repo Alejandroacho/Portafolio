53fb389a9d683368f16845e40768132e5179541d
Alejandro <alejandroacho@hotmail.com>
2020-08-15 00:46:17 +0200

    Views | Map modified

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -431,8 +431,8 @@
                 <p>+34 640 51 80 09</p>
               </div>
 
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d748.0860309768052!2d2.16537348814842!3d41.4100479986975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDHCsDI0JzM2LjIiTiAywrAwOSc1Ny4zIkU!5e0!3m2!1ses!2ses!4v1596839277759!5m2!1ses!2ses" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
-
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d40261.655416438836!2d2.154334628156025!3d41.4074139962043!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDHCsDI0JzM3LjgiTiAywrAwOSc1OS43IkU!5e0!3m2!1ses!2ses!4v1597445098415!5m2!1ses!2ses" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+              
             </div>
 
           </div>
